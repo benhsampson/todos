dedcff6eb4f64d49430fc3dc84c9f091c710a884
benhsampson <bensampsondev@gmail.com>
2022-04-26 17:58:10 +1000

    chore: clear junk

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,8 @@
 import dotenv from 'dotenv'
 import path from 'path'
-import { sillyString } from '@todos/shared'
 
 dotenv.config({ path: path.join(__dirname, '../../../.env') })
 
-export default function main() {
-  console.log('This is a rock solid boilerplate at this stage!')
-  console.log(sillyString)
-  console.log(process.env.TEST_VAR)
-}
+export default function main() {}
 
 main()
--- packages/shared/src/index.ts
+++
@@ -1,1 +0,0 @@
-export const sillyString = 'wow that is cool'
