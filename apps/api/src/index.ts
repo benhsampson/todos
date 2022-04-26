fbbd48689daec80a207247aa6a5b10a9bdc22798
benhsampson <bensampsondev@gmail.com>
2022-04-26 17:38:21 +1000

    chore: move to apps folder & add env vars

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv'
+import path from 'path'
 import { sillyString } from '@todos/shared'
 
+dotenv.config({ path: path.join(__dirname, '../../../.env') })
+
 export default function main() {
-  let badvar
   console.log('This is a rock solid boilerplate at this stage!')
   console.log(sillyString)
+  console.log(process.env.TEST_VAR)
 }
 
 main()
